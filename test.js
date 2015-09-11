e9032a06847356ad68d0901a8b0f0c45c9dd62d7
Cesar Polanco <cesar@iotum.com>
2015-09-10 17:01:28 -0700

    Trying out more verbosity

--- test.js
+++ test.js
@@ -310,20 +310,20 @@ function checkWeather(location){
   // that we can have send a message at a later time.
   get_weather_options.path = get_weather_options.path + location + "&units=imperial&APPID=3dc624292d12c666824acad2eec8bbcb"
   console.log(get_weather_options);
-  // var get_weather = https.request(get_weather_options, function(res){
-  //   var body = '';
-  //   res.on('data', function(d){
-  //     body += d;
-  //   });
-  //   res.on('end', function(){
-  //     var json_body = JSON.parse(body);
-  //     //console.log(json_body);
-  //     grabWeatherResults(json_body);
-  //     //console.log('BODY: ' + body);
-  //   });
-  // });
-  // //post_to_group.write(outputText);
-  // get_weather.end();
+  var get_weather = https.request(get_weather_options, function(res){
+    var body = '';
+    res.on('data', function(d){
+      body += d;
+    });
+    res.on('end', function(){
+      var json_body = JSON.parse(body);
+      console.log(json_body);
+      grabWeatherResults(json_body);
+      //console.log('BODY: ' + body);
+    });
+  });
+  //post_to_group.write(outputText);
+  get_weather.end();
   
   return converted;
 };
